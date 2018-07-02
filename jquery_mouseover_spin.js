0bd03a404c840a7588a6717d9e2740b0c41e3f93
KSpakes87 <40448897+KSpakes87@users.noreply.github.com>
2018-07-02 01:25:44 -0400

    Update jquery_mouseover_spin.js

--- jquery_mouseover_spin.js
+++ jquery_mouseover_spin.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $(".li1").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
   $(".li1").mouseout(function() {
     setTimeout(function() {
-      $(".li1").css("transition","0s");
+      $(".li1").css({"transition":"0s"});
       $(".li1").removeClass("spin");
     }, 1500);
   });
@@ -13,12 +13,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".li2").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
   $(".li2").mouseout(function() {
     setTimeout(function() {
-      $(".li2").css("transition","0s");
+      $(".li2").css({"transition":"0s"});
       $(".li2").removeClass("spin");
     }, 1500);
   });
@@ -26,12 +26,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".li3").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
-  $(".li3").mouseout(function() {
+  $(".li1").mouseout(function() {
     setTimeout(function() {
-      $(".li3").css("transition","0s");
+      $(".li3").css({"transition":"0s"});
       $(".li3").removeClass("spin");
     }, 1500);
   });
@@ -39,12 +39,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".li4").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
   $(".li4").mouseout(function() {
     setTimeout(function() {
-      $(".li4").css("transition","0s");
+      $(".li4").css({"transition":"0s"});
       $(".li4").removeClass("spin");
     }, 1500);
   });
@@ -52,12 +52,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".li5").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
-  $(".li5").mouseout(function() {
+  $(".li1").mouseout(function() {
     setTimeout(function() {
-      $(".li5").css("transition","0s");
+      $(".li5").css({"transition":"0s"});
       $(".li5").removeClass("spin");
     }, 1500);
   });
@@ -65,12 +65,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".li6").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
   $(".li6").mouseout(function() {
     setTimeout(function() {
-      $(".li6").css("transition","0s");
+      $(".li6").css({"transition":"0s"});
       $(".li6").removeClass("spin");
     }, 1500);
   });
@@ -78,12 +78,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".li7").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
   $(".li7").mouseout(function() {
     setTimeout(function() {
-      $(".li7").css("transition","0s");
+      $(".li7").css({"transition":"0s"});
       $(".li7").removeClass("spin");
     }, 1500);
   });
@@ -91,12 +91,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".li8").mouseover(function() {
-    $(this).css("transition","1.5s");
+    $(this).css({"transition":"1.5s"});
     $(this).addClass("spin");
   });
   $(".li8").mouseout(function() {
     setTimeout(function() {
-      $(".li8").css("transition","0s");
+      $(".li8").css({"transition":"0s"});
       $(".li8").removeClass("spin");
     }, 1500);
   });
